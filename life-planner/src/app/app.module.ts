f64f72510ba0cabe7edd9c59db32750d1b440c6f
mirmulk <mirrahsinul-mulk@uiowa.edu>
2018-02-04 21:58:18 -0600

    jquery import fix

--- life-planner/src/app/app.module.ts
+++ life-planner/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
