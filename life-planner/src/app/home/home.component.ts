9c843ce20da1702ca0182a52cd5dcb731b1af3f3
mirmulk <mirrahsinul-mulk@uiowa.edu>
2018-02-04 18:31:06 -0600

    sidenav

--- life-planner/src/app/home/home.component.ts
+++ life-planner/src/app/home/home.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    // Initialize collapse button
+    (<any>$('.button-collapse')).sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    // $('.collapsible').collapsible();
   }
 
 }
