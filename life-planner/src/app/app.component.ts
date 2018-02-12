1d1d6b7b756869f143c40bebd9cb14057a4b43cb
John Mauk <john-mauk@uiowa.edu>
2018-02-11 18:00:23 -0600

    Fixed nav bar issue for real. Seriously. Fixed but may not be optimal solution.

--- life-planner/src/app/app.component.ts
+++ life-planner/src/app/app.component.ts
@@ -14,13 +14,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     // Initialize collapse button
-    (<any>$('.button-collapse')).sideNav({
-        menuWidth: 300, // Default is 300
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
     // $('.collapsible').collapsible();
   }
-
 }
 
