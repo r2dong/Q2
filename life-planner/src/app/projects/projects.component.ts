90a6a80d6a3a9e46f69a766451c2a4ce685b1319
John Mauk <john-mauk@uiowa.edu>
2018-02-12 18:22:18 -0600

    starting on project creation. need to look into documentation a bit more though.

--- life-planner/src/app/projects/projects.component.ts
+++ life-planner/src/app/projects/projects.component.ts
@@ -17,12 +17,12 @@ export class ProjectsComponent implements OnInit {
     const projList = document.getElementById('project_list');
     const projName = (<HTMLInputElement>document.getElementById('project_new_name')).value;
     const projNewCard = '<div class=\"col s12 m6 l4\">\n' +
-      '    <div class=\"card blue darken-1\">\n' +
-      '      <div class=\"card-content white-text\">\n' +
-      '        <span class=\"card-title\">' + projName + '</span>\n' +
-      '      </div>\n' +
-      '    </div>\n' +
-      ' </div>';
+                        '    <div class=\"card blue darken-1\">\n' +
+                        '      <div class=\"card-content white-text\">\n' +
+                        '        <span class=\"card-title\">' + projName + '</span>\n' +
+                        '      </div>\n' +
+                        '    </div>\n' +
+                        ' </div>';
     const projCardNode = document.createTextNode(projNewCard);
     projList.appendChild(projCardNode);
   }
