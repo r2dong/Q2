01ced05619b20e4c28e11cacec9765ef41957ef2
John Mauk <john-mauk@uiowa.edu>
2018-02-13 19:41:07 -0600

    Updated projects components to be more static function calls. Need to connect to database to finish create.

--- life-planner/src/app/projects/projects.component.ts
+++ life-planner/src/app/projects/projects.component.ts
@@ -14,20 +14,28 @@ export class ProjectsComponent implements OnInit {
   ngOnInit() {
   }
 
-  newProject() {
-    console.log('hello clarice');
+  static projectsFromDatabase(){
+    ProjectsComponent.newProjectCard("From Database");
+  }
+
+  static newProjectFromForm(){
+    const projName = (<HTMLInputElement>document.getElementById('project_new_name')).value;
+    const form = (<HTMLFormElement>document.getElementById('project_create_form'));
+    ProjectsComponent.newProjectCard(projName);
+    form.reset();
+    return false;
+  }
+
+  static newProjectCard(projName) {
     const projList = document.getElementById('project_list');
-    var projName = (<HTMLInputElement>document.getElementById('project_new_name'));
     const projNewCard = '<div class=\"col s12 m6 l4\">\n' +
                         '    <div class=\"card blue darken-1\">\n' +
                         '      <div class=\"card-content white-text\">\n' +
-                        '        <span class=\"card-title\">' + projName.value + '</span>\n' +
+                        '        <span class=\"card-title\">' + projName + '</span>\n' +
                         '      </div>\n' +
                         '    </div>\n' +
                         ' </div>';
     projList.innerHTML = projList.innerHTML + projNewCard;
-    projName.innerText = "";
-    return false;
   }
 
 }
