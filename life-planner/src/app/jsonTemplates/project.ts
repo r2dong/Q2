7072fbc4b76a8d991b1a2c44d0d4fd5fd2360bd3
John Mauk <john-mauk@uiowa.edu>
2018-02-15 01:08:14 -0600

    Some minor explanation on the json object stuff.

--- life-planner/src/app/jsonTemplates/project.ts
+++ life-planner/src/app/jsonTemplates/project.ts
@@ -3,9 +3,28 @@ import { JsonObject, JsonProperty} from "json2typescript";
 @JsonObject
 export class Project {
 
-  @JsonProperty("name", String, false)
-  name: string = undefined;
+  // what the json object name will be, what type it should expect, and if it is required or not.
+  // Recommend to keep both the @JsonProperty and variable name the same to makes it easier.
+  @JsonProperty("name", String, true)
+  name: string = undefined;   // what you want to have this class use. could have been pokemon: string = undefined
   @JsonProperty("description", String, false)
   description: string = undefined;
 
+  /*
+  Example of how later on we can nest other json object interfaces into eachother.
+  Must put 'import { Task } from "./task"' however up top.
+  @JsonProperty("tasks", [Task], false)
+  tasks: Task[] = undefined;
+   */
+
 }
+
+/*
+  The "name" here is what goes inside of the @JsonProperty above.
+  json = [
+    {
+      "name":"test project",
+      "description":"This is cool!"
+    }
+  ]
+ */
