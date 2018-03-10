59c73e318b0149dcbe7c157692d0a7376c66fa65
r2dong <r2dong@yahoo.com>
2018-03-10 14:57:10 -0600

    Partially fixed tests, add initialize database environment.

--- life-planner/src/app/app.component.spec.ts
+++ life-planner/src/app/app.component.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { componentFactoryName } from '@angular/compiler';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      providers: [
+        AngularFirestore
       ],
+      imports: [
+        AngularFirestoreModule,
+        RouterModule,
+      ],
+      declarations: [
+        AppComponent,
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
