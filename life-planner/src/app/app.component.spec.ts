72d352d0a1655e75d2d2e7c89e08b5c56ebc5e77
r2dong <r2dong@yahoo.com>
2018-03-12 18:24:28 -0500

    style up login page with pictures in each quadrant.

--- life-planner/src/app/app.component.spec.ts
+++ life-planner/src/app/app.component.spec.ts
@@ -30,7 +30,7 @@ describe('AppComponent', () => {
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.debugElement.componentInstance; 
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'app'`, async(() => {
