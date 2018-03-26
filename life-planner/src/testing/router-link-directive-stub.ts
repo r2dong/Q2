0ae2e137be57063477144d81671d8c71268c9b74
r2dong <r2dong@yahoo.com>
2018-03-25 19:58:30 -0500

    add testing files

---
+++ life-planner/src/testing/router-link-directive-stub.ts
@@ -0,0 +1,12 @@
+@Directive({
+    selector: '[routerLink]',
+    host: { '(click)': 'onClick()' }
+  })
+  export class RouterLinkDirectiveStub {
+    @Input('routerLink') linkParams: any;
+    navigatedTo: any = null;
+  
+    onClick() {
+      this.navigatedTo = this.linkParams;
+    }
+  }
