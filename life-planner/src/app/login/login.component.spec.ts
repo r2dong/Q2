5a9a56c2f6bff92c25bcbaf373c592bafac70abc
r2dong <r2dong@yahoo.com>
2018-03-31 02:41:06 -0500

    finished testing login component

--- life-planner/src/app/login/login.component.spec.ts
+++ life-planner/src/app/login/login.component.spec.ts
@@ -11,14 +11,69 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import { RouterModule, Router } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
+import { Observable } from 'rxjs/Observable';
 
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+
+interface User {
+  uid: string;
+  email: string;
+  photoURL?: string;
+  displayName?: string;
+  favoriteColor?: string;
+}
+
+class AuthServiceStubFullClass {
+
+  _user: Observable<User>;
+  get user(): Observable<User> {
+    return this._user;
+  }
+  set user(toSet: Observable<User>) {
+    this._user = toSet;
+  }
+
+  constructor() {
+    this.user = Observable.of({
+      uid: "stubbedUid",
+      email: "stubbedEmail",
+      photoURL: "stubbedPhotoURL",
+      displayName: "aGiantStubUserName",
+      favoriteColor: "pink",
+    }) 
+  }
+
+  
+
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let el: DebugElement;
+  let auth: AuthService;
+  let authServiceStub : Partial<AuthService> = {
+    user: Observable.of({
+      uid: "stubbedUid",
+      email: "stubbedEmail",
+      photoURL: "stubbedPhotoURL",
+      displayName: "aGiantStubUserName",
+      favoriteColor: "pink",
+    }) 
+  }
+  let authServiceStub2 : Partial<AuthService> = {
+    get user() {
+      return Observable.of({
+        uid: "stubbedUid",
+        email: "stubbedEmail",
+        photoURL: "stubbedPhotoURL",
+        displayName: "aGiantStubUserName",
+        favoriteColor: "pink",
+      })
+    }
+  }
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -30,7 +85,7 @@ describe('LoginComponent', () => {
         {provide: Router, useClass: class {navigate = jasmine.createSpy("navigate")}},
         // Router,
         AuthGuard,
-        AuthService,
+        {provide: AuthService, useValue: new AuthServiceStubFullClass()},
         AngularFireAuth,
         AngularFirestore,
       ],
@@ -45,18 +100,25 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    auth = fixture.debugElement.injector.get(AuthService);
     //fixture.detectChanges();
     //el = fixture.debugElement.query(By.css('button'));
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('template should adjust to login status', () => {
+    let authSpy: jasmine.Spy = spyOnProperty(auth, 'user', 'get');
+    authSpy.and.returnValue(false);
     fixture.detectChanges();
     el = fixture.debugElement.query(By.css('button'));
     expect(el.nativeElement.textContent.trim()).toBe('keyboard_arrow_rightLogin with Google');
-    spyOnProperty(auth, 'user', 'get');
+    authSpy.and.callThrough();
+    fixture.detectChanges();
+    el = fixture.debugElement.query(By.css('button'));
+    expect(el.nativeElement.textContent.trim()).toBe('Logout');
   });
 });
