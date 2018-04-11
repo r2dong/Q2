c80a9e9a535a4f8aaf64fd2a850ba3ec39570105
mcaton <mcaton@medirevv.com>
2018-04-11 10:39:05 -0500

    added select-task and show-tasks

--- life-planner/src/app/tasks/tasks.module.ts
+++ life-planner/src/app/tasks/tasks.module.ts
@@ -7,6 +7,8 @@ import { AddTaskComponent } from './add-task/add-task.component';
 import { EditTaskComponent } from './edit-task/edit-task.component';
 import { SharedModule } from '../shared/shared.module';
 import { TaskService } from './task.service';
+import { SelectTaskComponent } from './select-task/select-task.component';
+import { ShowTasksComponent } from './show-tasks/show-tasks.component';
 
 @NgModule({
   imports: [
@@ -17,7 +19,9 @@ import { TaskService } from './task.service';
     TasksComponent,
     TaskDetailComponent,
     AddTaskComponent,
-    EditTaskComponent
+    EditTaskComponent,
+    SelectTaskComponent,
+    ShowTasksComponent
   ],
   providers: [
     TaskService
