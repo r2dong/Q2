890b13cf3ab5853ff64455faf0bc2c59e621fdf8
mcaton <mcaton@medirevv.com>
2018-04-23 10:07:26 -0500

    changed verbiage from new client added to new goal added.

--- life-planner/src/app/goals/add-goal/add-goal.component.ts
+++ life-planner/src/app/goals/add-goal/add-goal.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {GoalService} from '../goal.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import { FlashMessagesService } from 'angular2-flash-messages';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 import {GoalModel} from '../goal.model';
 import {Location} from '@angular/common';
-import {TaskModel, TaskWeight} from '../../tasks/task.model';
-import {TaskService} from '../../tasks/task.service';
+
 @Component({
   selector: 'app-add-goal',
   templateUrl: './add-goal.component.html',
@@ -19,16 +18,19 @@ export class AddGoalComponent implements OnInit {
     endDate: null,
   };
   @ViewChild('taskForm') form: any;
-  constructor(    private flashMessage: FlashMessagesService,
-                  private goalService: GoalService,
-                  private router: Router,
-                  private location: Location,
-                  private route: ActivatedRoute) { }
+
+  constructor(private flashMessage: FlashMessagesService,
+              private goalService: GoalService,
+              private router: Router,
+              private location: Location,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
 
   }
-  onSubmit({value, valid}: {value: GoalModel, valid: boolean}) {
+
+  onSubmit({value, valid}: { value: GoalModel, valid: boolean }) {
     if (!valid) {
       // Show error
       this.flashMessage.show('Please fill out the form correctly', {
@@ -38,7 +40,7 @@ export class AddGoalComponent implements OnInit {
       // Add new client
       this.goalService.addGoal(value);
       // Show message
-      this.flashMessage.show('New client added', {
+      this.flashMessage.show('New goal added', {
         cssClass: 'alert-success', timeout: 11000
       });
       // Redirect to tasks
